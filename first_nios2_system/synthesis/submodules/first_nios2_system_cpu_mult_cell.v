d1f4d28d110b38f270c6d488089f9b79d6ac9c01
Yong Wen Chua <yongwen110@imperial.ac.uk>
2013-02-15 15:35:27 +0000

    LUT Based Multiplier Test
    20 x 20
    100 times
    10.216

--- first_nios2_system/synthesis/submodules/first_nios2_system_cpu_mult_cell.v
+++ first_nios2_system/synthesis/submodules/first_nios2_system_cpu_mult_cell.v
@@ -37,93 +37,47 @@ module first_nios2_system_cpu_mult_cell (
   input            reset_n;
 
   wire    [ 31: 0] M_mul_cell_result;
-  wire    [ 31: 0] M_mul_cell_result_part_1;
-  wire    [ 15: 0] M_mul_cell_result_part_2;
   wire             mul_clr;
   assign mul_clr = ~reset_n;
-  altmult_add the_altmult_add_part_1
+  altmult_add the_altmult_add
     (
-      .aclr0 (mul_clr),
+      .aclr3 (mul_clr),
       .clock0 (clk),
-      .dataa (M_mul_src1[15 : 0]),
-      .datab (M_mul_src2[15 : 0]),
+      .dataa (M_mul_src1),
+      .datab (M_mul_src2[3 : 0]),
       .ena0 (1'b1),
-      .result (M_mul_cell_result_part_1)
+      .result (M_mul_cell_result)
     );
 
-  defparam the_altmult_add_part_1.addnsub_multiplier_pipeline_aclr1 = "ACLR0",
-           the_altmult_add_part_1.addnsub_multiplier_pipeline_register1 = "CLOCK0",
-           the_altmult_add_part_1.addnsub_multiplier_register1 = "UNREGISTERED",
-           the_altmult_add_part_1.dedicated_multiplier_circuitry = "YES",
-           the_altmult_add_part_1.input_register_a0 = "UNREGISTERED",
-           the_altmult_add_part_1.input_register_b0 = "UNREGISTERED",
-           the_altmult_add_part_1.input_source_a0 = "DATAA",
-           the_altmult_add_part_1.input_source_b0 = "DATAB",
-           the_altmult_add_part_1.intended_device_family = "CYCLONEIII",
-           the_altmult_add_part_1.lpm_type = "altmult_add",
-           the_altmult_add_part_1.multiplier1_direction = "ADD",
-           the_altmult_add_part_1.multiplier_aclr0 = "ACLR0",
-           the_altmult_add_part_1.multiplier_register0 = "CLOCK0",
-           the_altmult_add_part_1.number_of_multipliers = 1,
-           the_altmult_add_part_1.output_register = "UNREGISTERED",
-           the_altmult_add_part_1.port_addnsub1 = "PORT_UNUSED",
-           the_altmult_add_part_1.port_signa = "PORT_UNUSED",
-           the_altmult_add_part_1.port_signb = "PORT_UNUSED",
-           the_altmult_add_part_1.representation_a = "UNSIGNED",
-           the_altmult_add_part_1.representation_b = "UNSIGNED",
-           the_altmult_add_part_1.signed_pipeline_aclr_a = "ACLR0",
-           the_altmult_add_part_1.signed_pipeline_aclr_b = "ACLR0",
-           the_altmult_add_part_1.signed_pipeline_register_a = "CLOCK0",
-           the_altmult_add_part_1.signed_pipeline_register_b = "CLOCK0",
-           the_altmult_add_part_1.signed_register_a = "UNREGISTERED",
-           the_altmult_add_part_1.signed_register_b = "UNREGISTERED",
-           the_altmult_add_part_1.width_a = 16,
-           the_altmult_add_part_1.width_b = 16,
-           the_altmult_add_part_1.width_result = 32;
-
-  altmult_add the_altmult_add_part_2
-    (
-      .aclr0 (mul_clr),
-      .clock0 (clk),
-      .dataa (M_mul_src1[31 : 16]),
-      .datab (M_mul_src2[15 : 0]),
-      .ena0 (1'b1),
-      .result (M_mul_cell_result_part_2)
-    );
-
-  defparam the_altmult_add_part_2.addnsub_multiplier_pipeline_aclr1 = "ACLR0",
-           the_altmult_add_part_2.addnsub_multiplier_pipeline_register1 = "CLOCK0",
-           the_altmult_add_part_2.addnsub_multiplier_register1 = "UNREGISTERED",
-           the_altmult_add_part_2.dedicated_multiplier_circuitry = "YES",
-           the_altmult_add_part_2.input_register_a0 = "UNREGISTERED",
-           the_altmult_add_part_2.input_register_b0 = "UNREGISTERED",
-           the_altmult_add_part_2.input_source_a0 = "DATAA",
-           the_altmult_add_part_2.input_source_b0 = "DATAB",
-           the_altmult_add_part_2.intended_device_family = "CYCLONEIII",
-           the_altmult_add_part_2.lpm_type = "altmult_add",
-           the_altmult_add_part_2.multiplier1_direction = "ADD",
-           the_altmult_add_part_2.multiplier_aclr0 = "ACLR0",
-           the_altmult_add_part_2.multiplier_register0 = "CLOCK0",
-           the_altmult_add_part_2.number_of_multipliers = 1,
-           the_altmult_add_part_2.output_register = "UNREGISTERED",
-           the_altmult_add_part_2.port_addnsub1 = "PORT_UNUSED",
-           the_altmult_add_part_2.port_signa = "PORT_UNUSED",
-           the_altmult_add_part_2.port_signb = "PORT_UNUSED",
-           the_altmult_add_part_2.representation_a = "UNSIGNED",
-           the_altmult_add_part_2.representation_b = "UNSIGNED",
-           the_altmult_add_part_2.signed_pipeline_aclr_a = "ACLR0",
-           the_altmult_add_part_2.signed_pipeline_aclr_b = "ACLR0",
-           the_altmult_add_part_2.signed_pipeline_register_a = "CLOCK0",
-           the_altmult_add_part_2.signed_pipeline_register_b = "CLOCK0",
-           the_altmult_add_part_2.signed_register_a = "UNREGISTERED",
-           the_altmult_add_part_2.signed_register_b = "UNREGISTERED",
-           the_altmult_add_part_2.width_a = 16,
-           the_altmult_add_part_2.width_b = 16,
-           the_altmult_add_part_2.width_result = 16;
-
-  assign M_mul_cell_result = {M_mul_cell_result_part_1[31 : 16] +
-    M_mul_cell_result_part_2,
-    M_mul_cell_result_part_1[15 : 0]};
+  defparam the_altmult_add.addnsub_multiplier_pipeline_aclr1 = "ACLR3",
+           the_altmult_add.addnsub_multiplier_pipeline_register1 = "CLOCK0",
+           the_altmult_add.addnsub_multiplier_register1 = "UNREGISTERED",
+           the_altmult_add.dedicated_multiplier_circuitry = "NO",
+           the_altmult_add.input_register_a0 = "UNREGISTERED",
+           the_altmult_add.input_register_b0 = "UNREGISTERED",
+           the_altmult_add.input_source_a0 = "DATAA",
+           the_altmult_add.input_source_b0 = "DATAB",
+           the_altmult_add.intended_device_family = "CYCLONEIII",
+           the_altmult_add.lpm_type = "altmult_add",
+           the_altmult_add.multiplier1_direction = "ADD",
+           the_altmult_add.multiplier_aclr0 = "ACLR3",
+           the_altmult_add.multiplier_register0 = "CLOCK0",
+           the_altmult_add.number_of_multipliers = 1,
+           the_altmult_add.output_register = "UNREGISTERED",
+           the_altmult_add.port_addnsub1 = "PORT_UNUSED",
+           the_altmult_add.port_signa = "PORT_UNUSED",
+           the_altmult_add.port_signb = "PORT_UNUSED",
+           the_altmult_add.representation_a = "UNSIGNED",
+           the_altmult_add.representation_b = "UNSIGNED",
+           the_altmult_add.signed_pipeline_aclr_a = "ACLR3",
+           the_altmult_add.signed_pipeline_aclr_b = "ACLR3",
+           the_altmult_add.signed_pipeline_register_a = "CLOCK0",
+           the_altmult_add.signed_pipeline_register_b = "CLOCK0",
+           the_altmult_add.signed_register_a = "UNREGISTERED",
+           the_altmult_add.signed_register_b = "UNREGISTERED",
+           the_altmult_add.width_a = 32,
+           the_altmult_add.width_b = 4,
+           the_altmult_add.width_result = 32;
 
 
 endmodule
--- software/software_bsp/public.mk
+++ software/software_bsp/public.mk
@@ -156,9 +156,9 @@ SOPC_SYSID_FLAG += --sidp=0x1001038
 ELF_PATCH_FLAG  += --sidp 0x1001038
 
 # The SOPC Timestamp 
-# setting SOPC_TIMESTAMP is 1360937854
-SOPC_SYSID_FLAG += --timestamp=1360937854
-ELF_PATCH_FLAG  += --timestamp 1360937854
+# setting SOPC_TIMESTAMP is 1360941996
+SOPC_SYSID_FLAG += --timestamp=1360941996
+ELF_PATCH_FLAG  += --timestamp 1360941996
 
 # Small-footprint (polled mode) driver none 
 # setting altera_avalon_jtag_uart_driver.enable_small_driver is false
